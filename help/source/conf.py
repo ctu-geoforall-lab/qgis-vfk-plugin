b4d612f9221246e60551c5e1c6137722f119bf29
Martin Landa <landa.martin@gmail.com>
2022-11-08 21:41:21 +0100

    switch to bizstyle

--- help/source/conf.py
+++ help/source/conf.py
@@ -12,7 +12,6 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -94,7 +93,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
