030fd1b90755c5d4be2320d0da386e1c44b3df86
Martin Landa <landa.martin@gmail.com>
2023-02-17 12:18:49 +0100

    packaging: add dependencies

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf pywsdp
-pip3 install pywsdp -t . --no-deps --upgrade
-rm -rf tests pywsdp-*.dist-info
-find pywsdp/ -name __pycache__ -exec rm -rf {} \;
+pip3 install -r scripts/requirements.txt
+
+rm -rf pywsdp zeep requests_file.py isodate platformdirs requests_toolbelt
+pip3 install pywsdp zeep requests-file isodate platformdirs requests-toolbelt \
+     -t . --no-deps --upgrade
+# fix pywsdp (remove when 2.1.1 will be released)
+cp ../pywsdp/pywsdp/base/__init__.py pywsdp/base/__init__.py
+find pywsdp zeep isodate platformdirs requests_toolbelt \
+     -name __pycache__ | xargs rm -rf
 
 pb_tool zip
 
