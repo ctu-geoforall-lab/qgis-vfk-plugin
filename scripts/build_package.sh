0fadd4cb4e559dea64bc554e2b0f1aa5752e3c48
Martin Landa <landa.martin@gmail.com>
2023-07-10 18:33:00 +0200

    packaging: add modified pywsdp

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -10,6 +10,8 @@ LIB=$VENV/lib/python3.11/site-packages/
 cp -r $LIB/pywsdp $LIB/zeep $LIB/isodate $LIB/platformdirs $LIB/requests_toolbelt $LIB/requests_file.py .
 find pywsdp zeep isodate platformdirs requests_toolbelt \
      -name __pycache__ | xargs rm -rf
+# remove on new pywsdp release (see https://github.com/ctu-geoforall-lab/pywsdp/pull/51)
+cp ../pywsdp/pywsdp/base/__init__.py pywsdp/base/
 
 pb_tool zip
 
