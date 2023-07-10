6eb1dc94ae1b1e70b245ae949155e28582ee7fae
Martin Landa <landa.martin@gmail.com>
2023-07-10 18:26:29 +0200

    packaging: add missing requirement (requests_file)

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -7,7 +7,7 @@ source $VENV/bin/activate
 pip3 install -r scripts/requirements.txt 
 
 LIB=$VENV/lib/python3.11/site-packages/
-cp -r $LIB/pywsdp $LIB/zeep $LIB/isodate $LIB/platformdirs $LIB/requests_toolbelt .
+cp -r $LIB/pywsdp $LIB/zeep $LIB/isodate $LIB/platformdirs $LIB/requests_toolbelt $LIB/requests_file.py .
 find pywsdp zeep isodate platformdirs requests_toolbelt \
      -name __pycache__ | xargs rm -rf
 
