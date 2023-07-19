fe514df9e5e8ae6199b5bac443eef4d8f770c900
Martin Landa <landa.martin@gmail.com>
2023-07-19 10:44:36 +0200

    add attr library (missing on Windows)

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -6,9 +6,9 @@ source $VENV/bin/activate
 
 pip3 install -r scripts/requirements.txt 
 
-LIB=$VENV/lib/python3.11/site-packages/
-cp -r $LIB/pywsdp $LIB/zeep $LIB/isodate $LIB/platformdirs $LIB/requests_toolbelt $LIB/requests_file.py .
-find pywsdp zeep isodate platformdirs requests_toolbelt \
+LIB=$VENV/lib/python3.11/site-packages
+cp -r $LIB/pywsdp $LIB/zeep $LIB/isodate $LIB/platformdirs $LIB/requests_toolbelt $LIB/attr $LIB/requests_file.py .
+find pywsdp zeep isodate platformdirs requests_toolbelt attr \
      -name __pycache__ | xargs rm -rf
 # remove on new pywsdp release (see https://github.com/ctu-geoforall-lab/pywsdp/pull/51)
 cp ../pywsdp/pywsdp/base/__init__.py pywsdp/base/
