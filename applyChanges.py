78d42d8fc6ed53bdf709d65c5c6975eeb1ca8a6a
Martin Landa <landa.martin@gmail.com>
2024-01-22 22:57:14 +0100

    remove future imports

--- applyChanges.py
+++ applyChanges.py
@@ -21,7 +21,6 @@
  *                                                                         *
  ***************************************************************************/
 """
-from __future__ import print_function
 from builtins import str
 from builtins import zip
 
--- searchFormController.py
+++ searchFormController.py
@@ -20,7 +20,6 @@
  *                                                                         *
  ***************************************************************************/
 """
-from __future__ import absolute_import
 from builtins import range
 from builtins import object
 
